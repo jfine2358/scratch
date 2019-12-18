ac2e87e4d8a5481be62f78cbc70e38c3c5d51ec7
Jonathan Fine <jfine2358@gmail.com>
2019-12-18 13:52:52 +0000

    Rewrite the Javascript, to be clearer.

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
 
 // Based on code on page http://asciimath.org/
 window.MathJax = {
+
     "fast-preview": {
 	disabled: true
     },
+
     AuthorInit: function() {
 	MathJax.Hub.Register.StartupHook('End', function() {
+
 	    MathJax.Hub.processSectionDelay = 0
-	    var demoSource = document.getElementById('src')
-	    var demoRendering = document.getElementById('tgt')
-	    var math = MathJax.Hub.getAllJax('tgt')[0]
-	    demoSource.addEventListener('input', function() {
-	      MathJax.Hub.Queue(['Text', math, demoSource.value])
+
+	    // Get nodes src and jax.
+	    var src = document.getElementById('src')
+	    var tgt = document.getElementById('tgt')
+	    var jax = MathJax.Hub.getAllJax(tgt)[0]
+
+	    // Have input at src queue update on jax.
+	    src.addEventListener('input', function() {
+	      MathJax.Hub.Queue(['Text', jax, src.value])
 	    })
+
 	})
     }
 }
