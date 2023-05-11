e03d78f869cacacbcedfde86dce15fcd9577de24
Jonathan Fine <jfine2358@gmail.com>
2023-05-11 17:16:40 +0100

    Add writing of html_log.txt files to example.py. It's a hack.

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
+import gzip
 import os
 import subprocess
 import time
+from html.parser import HTMLParser
 from tools import get_random_arxiv_id
 from tools.arxivid import ArxivId
 from tools import Store
@@ -35,6 +37,8 @@ key = 'html_log'
 key = 'pdf'
 for id in random_ids:
 
+    break                       # Already populated.
+
     file_name = f'{id.dir_name}.{key}'
     file_path = os.path.join(articles.root, id.dir_name, file_name)
 
@@ -46,3 +50,86 @@ for id in random_ids:
     completed  = subprocess.run(args)
     results.append(completed)
     time.sleep(10)
+
+key = 'source'
+for id in random_ids:
+    break                       # Done already.
+    tgt_path = os.path.join(articles.root, id.dir_name, key)
+    os.mkdir(tgt_path)
+
+
+key = 'source'
+results = []
+for id in random_ids:
+    break                       # Done already.
+
+    tar_name = f'{id.dir_name}.{key}'
+    tar_path = os.path.join(articles.root, id.dir_name, tar_name)
+    tgt_path = os.path.join(articles.root, id.dir_name, key)
+
+#    os.mkdir(tgt_path)
+
+    url = getattr(id.urls, key)
+
+    args = ['tar', '-xvzf', tar_path, '-C', tgt_path]
+    print(args)
+    completed  = subprocess.run(args)
+    results.append(completed)
+
+# results[2], results[4] fail with returncode 2.
+
+
+key = 'source'
+results = []
+for id in [random_ids[2], random_ids[4]]:
+    break                       # Already done.
+    tar_name = f'{id.dir_name}.{key}'
+    # so-called tar_path is gzip path.
+    tar_path = os.path.join(articles.root, id.dir_name, tar_name)
+    tgt_dir = os.path.join(articles.root, id.dir_name, key)
+    tgt_path = os.path.join(tgt_dir, f'{id.dir_name}.tex')
+
+    url = getattr(id.urls, key)
+
+    args = ['gunzip', '--decompress', '--keep', tar_path, tgt_path]
+    print(args)
+
+    import gzip
+    with gzip.open(tar_path, 'rb') as src:
+        with open(tgt_path, 'wb') as tgt:
+            tgt.write(src.read())
+
+key = 'html_log'
+results = []
+for id in random_ids:
+
+    class LogParser(HTMLParser):
+
+        def handle_data(self, data):
+            data = data.rstrip()
+            data = data.lstrip('\n')
+            if data:
+                append(data)
+
+
+    html_log_name = f'{id.dir_name}.{key}'
+    html_log_path = os.path.join(articles.root, id.dir_name, html_log_name)
+    html_log = open(html_log_path).read()
+
+
+    html_log_txt_name = f'{id.dir_name}.{key}.txt'
+    html_log_txt_path = os.path.join(articles.root, id.dir_name,
+                                     html_log_txt_name)
+
+    # print(html_log_txt_path)
+    # print(len(html_log))
+
+    parser = LogParser()
+    tmp = []
+    append = tmp.append
+    parser.feed(html_log)
+    del parser                  # Don't use it again.
+
+    with open(html_log_txt_path, 'w') as fd:
+        for line in tmp:
+            fd.write(f'{line}\n')
